0182608d109fdd72569f967b1d98b8e86e17d7f2
chevisutton <cheviksutton@gmail.com>
2018-08-01 22:37:59 -0600

    Most HTML is done

--- src/TimerCard.js
+++ src/TimerCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import CountUp, { startAnimation } from 'react-countup';
 
 class TimerCard extends Component {
     render(){
@@ -6,9 +8,14 @@ class TimerCard extends Component {
             <div class="tForm">
                 <h1>Mow the yard</h1>
                 <h3>House Chores</h3>
-                <p>01:05.45</p>
+                {/*<p>01:05.45</p>*/}
+                <CountUp className="CountUp" start={0} end={100} duration={3} ref={(countUp) => {
+                    this.myCountUp = countUp;
+                }} />
                 <p className='formBtn'>
-                    <button>Start</button>
+                    <button className="Button" onClick={(event) => {
+                        startAnimation(this.myCountUp);
+                    }}>Start</button>
                     <button>Edit</button>
                     <button>Delete</button>
                 </p>
